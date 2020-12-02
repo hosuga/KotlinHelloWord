b444b71cb97d9f32ab18886288db1054a36320c6
大須賀寛人 <hosuga@itrealize.co.jp>
2020-12-02 10:40:06 +0900

    add_BuildVariants

--- app/src/kind02/java/com/example/kotlinhelloworld/kind02/MainActivity.kt
+++ app/src/kind02/java/com/example/kotlinhelloworld/kind02/MainActivity.kt
@@ -1,27 +1,31 @@
-package com.example.kotlinhelloworld
+package com.example.kotlinhelloworld.kind02
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AlertDialog
 import android.os.Bundle
 import android.text.TextUtils.isEmpty
 import android.widget.TextView
 import android.widget.EditText
 import android.widget.Button
+import com.example.kotlinhelloworld.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val textView = findViewById<TextView>(R.id.text_view) as TextView
         val editText = findViewById<EditText>(R.id.edit_text) as EditText
         val button = findViewById<Button>(R.id.button) as Button
-
 
         button.setOnClickListener {
             var name = WORLD
             val userName = editText.getText().toString()
             if (!isEmpty(userName.trim())) {
-                name = userName
+                if (validate(userName)) {
+                    name = userName
+                }
             }
             textView.setText("Hello $name!")
         }
@@ -29,5 +33,18 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         const val WORLD: String = "World"
+        val ERROR_MESSAGES = mapOf("ERROR_TITLE" to "ERROR!", "TOO_LONG_NAME" to "10文字以内で入力して下さい。")
+    }
+
+    private fun validate(userName : String) : Boolean{
+        if(userName.length > 10){
+            AlertDialog.Builder(this)
+                .setTitle(ERROR_MESSAGES["ERROR_TITLE"])
+                .setMessage(ERROR_MESSAGES["TOO_LONG_NAME"])
+                .setPositiveButton("OK"){ dialog, which -> }
+                .show()
+            return false
+        }
+        return true
     }
 }
